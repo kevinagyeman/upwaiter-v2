ad5a4ea0cfcaaaa114eee0baf7a98e938970e163
kevinagyeman <kevinagyemann@gmail.com>
2025-01-13 20:15:33 +0100

    add graphql post

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,7 +1,8 @@
+import { routing } from '@/i18n/routing';
+import { ApolloWrapper } from '@/lib/apollo-wrapper';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { routing } from '@/i18n/routing';
 
 export default async function LocaleLayout({
   children,
@@ -10,20 +11,17 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound();
   }
 
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
-          {children}
+          <ApolloWrapper>{children}</ApolloWrapper>
         </NextIntlClientProvider>
       </body>
     </html>
---
+++ frontend/src/app/[locale]/register/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+
+const REGISTER_MUTATION = gql`
+  mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+      username
+    }
+  }
+`;
+
+export default function Register() {
+  const [mutateFunction, { data, loading, error }] =
+    useMutation(REGISTER_MUTATION);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    mutateFunction({
+      variables: {
+        username: username,
+        password: password,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' onChange={(e) => setUsername(e.target.value)} />
+        <input type='password' onChange={(e) => setPassword(e.target.value)} />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
